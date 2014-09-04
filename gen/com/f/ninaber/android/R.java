872b764d1eeb81808471b4dd717e2c5d407e87b3
ferdinb <ferdi.ninaber@gmail.com>
2014-09-04 17:00:39 +0700

    add

--- gen/com/f/ninaber/android/R.java
+++ gen/com/f/ninaber/android/R.java
@@ -32,22 +32,28 @@ public final class R {
         public static final int text_size_small=0x7f05000a;
     }
     public static final class drawable {
-        public static final int background_drawable_white_gray=0x7f020000;
-        public static final int btn_add_reminder=0x7f020001;
-        public static final int ic_add_reminder_off=0x7f020002;
-        public static final int ic_add_reminder_on=0x7f020003;
-        public static final int ic_add_task_message=0x7f020004;
-        public static final int ic_add_task_time=0x7f020005;
-        public static final int ic_add_task_title=0x7f020006;
-        public static final int ic_drop_down=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int ic_logo=0x7f020009;
+        public static final int background_drawable_round_red=0x7f020000;
+        public static final int background_drawable_white_gray=0x7f020001;
+        public static final int btn_add_reminder=0x7f020002;
+        public static final int ic_add_reminder_off=0x7f020003;
+        public static final int ic_add_reminder_on=0x7f020004;
+        public static final int ic_add_task_message=0x7f020005;
+        public static final int ic_add_task_time=0x7f020006;
+        public static final int ic_add_task_title=0x7f020007;
+        public static final int ic_camera=0x7f020008;
+        public static final int ic_drop_down=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int ic_logo=0x7f02000b;
+        public static final int ic_recorder=0x7f02000c;
+        public static final int ic_sound_recorder=0x7f02000d;
     }
     public static final class id {
-        public static final int action_bar_add=0x7f09000b;
-        public static final int action_bar_icon=0x7f090009;
-        public static final int action_bar_title=0x7f09000a;
-        public static final int action_settings=0x7f09000c;
+        public static final int action_bar_add=0x7f09000d;
+        public static final int action_bar_icon=0x7f09000b;
+        public static final int action_bar_title=0x7f09000c;
+        public static final int action_settings=0x7f09000e;
+        public static final int activity_add_task_cancel=0x7f090009;
+        public static final int activity_add_task_save=0x7f090008;
         public static final int add_task_date=0x7f090001;
         public static final int add_task_date_group=0x7f090000;
         public static final int add_task_notes=0x7f090007;
@@ -56,7 +62,7 @@ public final class R {
         public static final int add_task_time_group=0x7f090003;
         public static final int add_task_timer_image=0x7f090004;
         public static final int add_task_title=0x7f090006;
-        public static final int container=0x7f090008;
+        public static final int container=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_add_task=0x7f030000;
@@ -69,11 +75,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f060009;
         public static final int hello_world=0x7f060001;
         public static final int notes=0x7f060004;
         public static final int off=0x7f060007;
         public static final int on=0x7f060006;
         public static final int repeat=0x7f060005;
+        public static final int save=0x7f060008;
         public static final int title=0x7f060003;
     }
     public static final class style {
